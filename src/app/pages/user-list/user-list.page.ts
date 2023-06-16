311f111251168d8894c19178dd41ad408d9843c5
seb <contact@smaloron.eu>
2023-06-16 11:34:58 +0200

    randomUser : recherche

--- src/app/pages/user-list/user-list.page.ts
+++ src/app/pages/user-list/user-list.page.ts
@@ -1,7 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
-const URL = 'http://randomuser.me/api?results=10';
+const URL = 'https://randomuser.me/api';
+
+interface UserSearch {
+  numberOfResults: number;
+  gender: string;
+  nationality: string
+}
 
 @Component({
   selector: 'app-user-list',
@@ -14,20 +20,47 @@ export class UserListPage implements OnInit {
 
   userDetails: any = null;
 
+  search: UserSearch = {
+    numberOfResults: 6,
+    gender: 'male',
+    nationality: 'dk'
+  }
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get(URL).subscribe(
-      (response: any) => {
-        console.log(response);
-        this.userList = response.results;
-      }
-    )
+    this.loadMoreUsers();
   }
 
   getUser(index: number) {
     this.userDetails = this.userList[index];
     console.log(this.userDetails);
+  }
+
+  loadMoreUsers(even: any = null, prepend = false) {
+
+    const search = new HttpParams()
+      .set('results', this.search.numberOfResults)
+      .set('gender', this.search.gender)
+      .set('nat', this.search.nationality);
+
+    this.http.get(URL, { params: search }).subscribe(
+      (response: any) => {
+        if (prepend) {
+          this.userList = response.results.concat(this.userList);
+        } else {
+          this.userList = this.userList.concat(response.results);
+        }
+        if (even) {
+          even.target.complete();
+        }
+      }
+    );
+  }
+
+  searchUser() {
+    this.userList = [];
+    this.loadMoreUsers();
   }
 
 }
